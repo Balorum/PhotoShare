3f6ff640f29be943dd27abd56d161ae9a1b1f658
Balorum <Remeshevskyi.Nikita03@gmail.com>
2024-02-26 16:36:51 +0000

    fix db#2

--- src/database/db.py
+++ src/database/db.py
@@ -7,7 +7,7 @@ from src.conf.config import settings
 
 
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
-print(SQLALCHEMY_DATABASE_URL)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
