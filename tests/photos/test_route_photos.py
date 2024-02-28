496e1b821571bc18c519f18b7ae37ab8e6de6361
Balorum <Remeshevskyi.Nikita03@gmail.com>
2024-02-28 01:03:15 +0000

    add repository test

--- tests/photos/test_route_photos.py
+++ tests/photos/test_route_photos.py
@@ -1,9 +1,20 @@
 from unittest.mock import MagicMock, patch
-
+from PIL import Image
+import io
 import pytest
 
+from fastapi import UploadFile
 from src.database.models import User
 from src.services.auth import auth_service
+
+
+def create_file():
+    file_data = io.BytesIO()
+    image = Image.new("RGB", size=(100, 100), color=(255, 0, 0))
+    image.save(file_data, "jpeg")
+    file_data.seek(0)
+    file = UploadFile(file_data)
+    return file
 
 
 @pytest.fixture()
@@ -11,28 +22,39 @@ def token(client, user, session, monkeypatch):
     mock_send_email = MagicMock()
     monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
     client.post("/api/auth/signup", json=user)
-    current_user: User = session.query(User).filter(User.email == user.get('email')).first()
+    current_user: User = (
+        session.query(User).filter(User.email == user.get("email")).first()
+    )
     current_user.confirmed = True
     session.commit()
     response = client.post(
         "/api/auth/login",
-        data={"username": user.get('email'), "password": user.get('password')},
+        data={"username": user.get("email"), "password": user.get("password")},
     )
     data = response.json()
     return data["access_token"]
 
 
-
-
 def test_create_photo(client, token):
-    with patch.object(auth_service, 'r') as r_mock:
+    with patch.object(auth_service, "r") as r_mock:
         r_mock.get.return_value = None
+        data = {
+            "title": "test_post",
+            "description": "test_post",
+            "tags": ["test_post"],
+        }
+        file = create_file()
+        print(type(create_file))
+
         response = client.post(
             "/api/photos/create/",
-            headers={"Authorization": f"Bearer {token}"}
+            headers={"Authorization": f"Bearer {token}"},
+            data=data,
+            files={"file": ("test.jpg", file, "image/jpeg")},
         )
         assert response.status_code == 201, response.text
         data = response.json()
         assert data["title"] == "test_photo"
         assert data["description"] == "lorem ipsum"
+        assert isinstance(data["description"], str)
         assert "id" in data
--- tests/photos/unit_test_repository_photos.py
+++ tests/photos/unit_test_repository_photos.py
@@ -2,13 +2,14 @@ import collections
 import pathlib
 from PIL import Image
 import io
+
 collections.Callable = collections.abc.Callable
 import unittest
 from unittest.mock import MagicMock
 from fastapi import UploadFile
 from sqlalchemy.orm import Session
 from datetime import datetime
-from src.database.models import Photo, User
+from src.database.models import Photo, User, Tag
 from src.schemas.photos import PhotoBase
 from src.repository.photos import (
     get_photos,
@@ -17,25 +18,40 @@ from src.repository.photos import (
     get_photo_by_id,
     create_photo,
     remove_photo,
-    update_photo
+    update_photo,
 )
+
+
+def create_file():
+    file_data = io.BytesIO()
+    image = Image.new("RGB", size=(100, 100), color=(255, 0, 0))
+    image.save(file_data, "jpeg")
+    file_data.seek(0)
+    file = UploadFile(file_data)
+    return file
 
 
 class TestContact(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
         self.session = MagicMock(spec=Session)
-        self.user = User(id=1, username='test_user', password="qwerty", confirmed=True, role='user')
+        self.user = User(
+            id=1, username="test_user", password="qwerty", confirmed=True, role="user"
+        )
 
     async def test_get_photos(self):
         photos = [Photo(), Photo(), Photo()]
-        self.session.query.return_value.offset.return_value.limit.return_value.all.return_value = photos
+        self.session.query.return_value.offset.return_value.limit.return_value.all.return_value = (
+            photos
+        )
         result = await get_photos(10, 0, self.session)
         self.assertEqual(result, photos)
 
     async def test_get_user_photos(self):
         photos = [Photo(user_id=1), Photo(user_id=1), Photo()]
-        self.session.query.return_value.filter.return_value.offset.return_value.limit.return_value.all.return_value = photos
+        self.session.query.return_value.filter.return_value.offset.return_value.limit.return_value.all.return_value = (
+            photos
+        )
         result = await get_user_photos(10, 0, self.user, self.session)
         self.assertEqual(result, photos)
 
@@ -58,28 +74,53 @@ class TestContact(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(result, photo)
         self.user.role = "user"
         photo = Photo(id=1, user_id=2)
-        self.session.query.return_value.filter.return_value.filter.return_value.first.return_value = photo
+        self.session.query.return_value.filter.return_value.filter.return_value.first.return_value = (
+            photo
+        )
         result = await get_photo_by_id(1, self.user, self.session)
         self.assertEqual(result, photo)
 
-    # async def test_create_photo(self):
-    #     title = 'photo_title'
-    #     description = 'photo_description'
-    #     file = 'photo_file'
-    #     photo = Photo(title=title, description=description)
-    #     result = await create_photo(title, description, None, self.user, file, self.session)
-    #     self.assertEqual(result, photo)
+    async def test_create_photo(self):
 
-    # async def test_update_contact(self):
-    #     photo = Photo(id=1, user_id=1, title='photo_title', description='photo_description', image_url = 'photo_file')
-    #     self.session.query.return_value.filter.return_value.filter.return_value.first.return_value = Photo(id=1,
-    #                                                                                                        user_id=1,
-    #                                                                                                        title='photo_2',
-    #                                                                                                        description='23131')
-    #     result = await update_photo(photo.id, photo.title, photo.description, None, self.user, None, self.session)
-    #     self.assertEqual(photo,result)
+        title = "photo_title"
+        description = "photo_description"
+        tags = ["test_tag1", "test_tag2", "test_tag3"]
+        file = create_file()
+
+        result = await create_photo(
+            title, description, tags, self.user, file, self.session
+        )
+        assert isinstance(result.image_url, str)
+        assert result.title == title
+        assert result.description == description
+
+    async def test_update_contact(self):
+        id = 1
+        user_id = 1
+        title = "photo_title"
+        description = "photo_description"
+        tags = []
+        image_url = "photo_file"
+        file = create_file()
+
+        result = await update_photo(
+            id,
+            title,
+            description,
+            tags,
+            self.user,
+            file,
+            self.session,
+        )
+        assert isinstance(result.image_url, str)
+        assert image_url != result.image_url
+        assert result.title == title
+        assert result.description == description
+
     async def test_delete_photo_by_id(self):
         photo = Photo(id=1, user_id=1)
-        self.session.query.return_value.filter.return_value.filter.return_value.first.return_value = photo
+        self.session.query.return_value.filter.return_value.filter.return_value.first.return_value = (
+            photo
+        )
         result = await remove_photo(1, self.user, self.session)
         self.assertEqual(result, photo)
